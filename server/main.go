552410fafdcc06f124be0c31fe4a8aab6766d114
ajagnic <adrian.agnic@hotmail.com>
2019-06-11 17:01:10 -0700

    save: start logger and server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("test")
+	logger := log.New(nil, "", 0)
+	logger.Fatal(http.ListenAndServe("127.0.0.1:8001", nil))
 }
