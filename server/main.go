ec8dead9d6fc933d59c2131bf4618b738674a4b0
Adrian Agnic <adrian@tura.io>
2019-06-12 23:26:12 -0700

    minor

--- server/main.go
+++ server/main.go
@@ -42,6 +42,8 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 
 			_, err = io.CopyBuffer(file, part, nil)
 			output.Pf("CopyBuffer: %v", err, false)
+
+			http.ServeFile(w, r, "./static/index.html")
 		}
 	} else {
 		http.ServeFile(w, r, "./static/error.html")
