41d0d607c8e3aff16aed469361091cd4382b8d93
ajagnic <adrian.agnic@hotmail.com>
2019-06-20 14:07:02 -0700

    config page + route, scheduler was needlessly complicated, rethinking

--- server/main.go
+++ server/main.go
@@ -9,11 +9,26 @@ import (
 	"os/signal"
 
 	"github.com/ajagnic/ARImport/output"
-	"github.com/ajagnic/ARImport/scheduler"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/index.html")
+}
+
+func configHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		runTime := r.FormValue("runtime")
+
+		file, err := os.OpenFile("./scheduler/config.txt", os.O_WRONLY, 0644)
+		output.Pf("Could not open config file: %v", err, false)
+
+		if err == nil {
+			defer file.Close()
+			file.WriteString(runTime)
+		}
+	}
+	http.ServeFile(w, r, "./static/config.html")
 }
 
 //contentHandler serves static files based on URL path.
@@ -24,15 +39,15 @@ func contentHandler(w http.ResponseWriter, r *http.Request) {
 //postHandler streams up form data from POST and saves to local file.
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		rdr, _ := r.MultipartReader()
-		// output.Pf("File reader: %v", err, false)
+		rdr, err := r.MultipartReader()
+		output.Pf("File reader: %v", err, false)
 
 		for { //Loop file parts until EOF and place in write buffer.
 			part, err := rdr.NextPart()
 			if err == io.EOF {
 				break
 			} else {
-				// output.Pf("File parts: %v", err, false)
+				output.Pf("File parts: %v", err, false)
 			}
 
 			if part.FileName() == "" {
@@ -40,14 +55,14 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 			}
 
 			file, err := os.OpenFile("./static/csv/"+part.FileName(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			// output.Pf("", err, true)
+			output.Pf("", err, true)
 			defer file.Close()
 
 			_, err = io.CopyBuffer(file, part, nil)
-			// output.Pf("CopyBuffer: %v", err, false)
+			output.Pf("CopyBuffer: %v", err, false)
 		}
 
-		http.ServeFile(w, r, "./static/index.html") //TODO: implement this route w/ custom handler (middleware), test if process continues while http serves new
+		http.ServeFile(w, r, "./static/index.html")
 	} else {
 		http.ServeFile(w, r, "./static/error.html")
 		output.Log.Printf("Invalid method in /store: %v", r.Method)
@@ -56,9 +71,6 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SCHEDULER
-	timekill := make(chan bool, 1)
-	scheduler.Config()
-	go scheduler.Run(timekill)
 
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SERVER
 	addr := ":8001"
@@ -70,23 +82,23 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/static/", contentHandler)
 	http.HandleFunc("/store", postHandler)
+	http.HandleFunc("/config", configHandler)
 
-	//sigint listens for interrupt signal and calls http.Server.Shutdown.
+	//sigint listens for interrupt signal then cleans up main()
 	sigint := make(chan os.Signal, 1)
 	signal.Notify(sigint, os.Interrupt)
 
 	go func() {
 		fmt.Printf("Starting server on URL/Port: %v\n", addr)
-		_ = srv.ListenAndServe()
-		// output.Pf("", err, true)
+		err := srv.ListenAndServe()
+		output.Pf("", err, true)
 	}()
 
 	<-sigint
-	_ = srv.Shutdown(context.Background())
-	// output.Pf("", err, true)
 
-	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	timekill <- true
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CLEANUP
+	err := srv.Shutdown(context.Background())
+	output.Pf("", err, true)
 	output.Close()
 	fmt.Println("Server gracefully shutdown.")
 }
