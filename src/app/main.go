18ced1d3c15e9c5d8a70b55ee7416919c6a3a925
ajagnic <adrian.agnic@hotmail.com>
2019-06-25 16:36:11 -0700

    minor

--- src/app/main.go
+++ src/app/main.go
@@ -1,54 +1,53 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"net/http"
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/ajagnic/ARImport/src/output"
 	"github.com/ajagnic/ARImport/src/scheduler"
+	"github.com/ajagnic/ARImport/src/server"
 )
 
 func main() {
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SCHEDULER
-	reinit = make(chan bool, 1)
+	reinit := make(chan bool, 1)
 	killsched := make(chan bool, 1)
-	addr := scheduler.Config()
+	_ = scheduler.Config()
 	go scheduler.EventListener(reinit, killsched)
 
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SERVER
-	srv := http.Server{
-		Addr:     addr,
-		ErrorLog: output.Log,
-	}
+	server.Run()
+	// srv := http.Server{
+	// 	Addr:     addr,
+	// 	ErrorLog: output.Log,
+	// }
 
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/static/", contentHandler)
-	http.HandleFunc("/store", postHandler)
-	http.HandleFunc("/config", configHandler)
+	// http.HandleFunc("/", indexHandler)
+	// http.HandleFunc("/static/", contentHandler)
+	// http.HandleFunc("/store", postHandler)
+	// http.HandleFunc("/config", configHandler)
 
 	//sigint listens for interrupt signal then cleans up main()
 	sigint := make(chan os.Signal, 1)
 	signal.Notify(sigint, os.Interrupt)
 
-	go func() {
-		fmt.Printf("Starting server on URL/Port: %v\n", addr)
-		err := srv.ListenAndServe()
-		output.Pf("", err, true)
-	}()
+	// go func() {
+	// 	fmt.Printf("Starting server on URL/Port: %v\n", addr)
+	// 	err := srv.ListenAndServe()
+	// 	output.Pf("", err, true)
+	// }()
 
 	<-sigint
 
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CLEANUP
 	killsched <- true
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
-	defer cancel()
-	err := srv.Shutdown(ctx)
-	output.Pf("", err, true)
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	// defer cancel()
+	// err := srv.Shutdown(ctx)
+	// output.Pf("", err, true)
 
 	output.Close()
 	fmt.Println("Server gracefully shutdown.")
--- src/server/router.go
+++ src/server/router.go
@@ -11,7 +11,7 @@ import (
 
 var reinit chan bool
 
-func init() {
+func Run() {
 	return
 }
 
